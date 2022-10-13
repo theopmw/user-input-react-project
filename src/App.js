cfb0b66d996a5b5d0ec1cc01a87079194de35758
theopmw <theowrightdev@gmail.com>
2022-10-13 10:32:21 +0100

    Add UsersList component and CSS styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
   return (
     <div>
+      {/* Block for getting the user data */}
       <AddUser />
+      {/* Block for outputting the user data and set users prop to empty array */}
+      <UsersList users={[]} />
     </div>
   );
 }
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Card from '../UI/Card';
+import classes from './UsersList.module.css';
+
+const UsersList = (props) => {
+  // Return a list of users
+  // Use props to retrieve array of users as data
+  // Use props.users.map() to map the array of users to an array of JSX elements (<li></li>) that contain the user data
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
